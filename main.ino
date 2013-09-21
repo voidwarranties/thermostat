2da45877b1e6a6969aacaba8d2feba4b7bb286c3
Ward De Ridder <git@warddr.eu>
2013-09-21 17:41:20 +0200

    multiple coltrole boards

--- main.ino
+++ main.ino
@@ -2,50 +2,57 @@
   Void-Josto thermostat
 */
 
-int B1L1 = 32;
-int B1L2 = 24;
-int B1push=30;
+const int L1[] = {32, 33};
+const int L2[] = {24, 25};
+const int push[] = {30, 31};
 
 int time = 10; //(in seconden)
-int count1 = 0; //loopt in seconden
+int count[] = {0, 0}; //loopt in seconden
 
 int secondenteller = 0;
 
 void setup() {                
-  pinMode(B1L1, OUTPUT);
-  pinMode(B1L2, OUTPUT);
-  pinMode(B1push, INPUT);
-
+  pinMode(L1[0], OUTPUT);
+  pinMode(L2[0], OUTPUT);
+  pinMode(push[0], INPUT);
+  pinMode(L1[1], OUTPUT);
+  pinMode(L2[1], OUTPUT);
+  pinMode(push[1], INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   delay(100); //we loopen 10 keer / seconde
   
     //teller aan en uit zetten. Je kan minstens 1 seconde drukken voor je aan en uit gaat.
-    if (digitalRead(B1push) == 1){
-      if (count1 == 0){
-       count1 = time;
-       digitalWrite(B1L1, true);
+    for(int i = 0; i<=1; i++){
+    Serial.println(push[i], DEC);
+    if (digitalRead(push[i]) == 1){
+      if (count[i] == 0){
+       count[i] = time;
+       digitalWrite(L1[i], true);
       }else{
-       if (count1 < (time-1)){
-         count1 = 1;
+       if (count[i] < (time-1)){
+         count[i] = 1;
          secondenteller = 0;
-         digitalWrite(B1L1, false);
+         digitalWrite(L1[i], false);
        }
       } 
+    }
     }
 
   secondenteller++; //onderstaand deel loopt 1 keer / seconde
   if (secondenteller == 10){
-    secondenteller = 0;
-    if (count1 > 0){
-      count1--;
+  secondenteller = 0;
+  for(int i = 0; i<=1; i++){
+    if (count[i] > 0){
+      count[i]--;
     }
-    if (count1 > 0){
-      digitalWrite(B1L1, true);
+    if (count[i] > 0){
+      digitalWrite(L1[i], true);
     }else{
-      digitalWrite(B1L1, false);
+      digitalWrite(L1[i], false);
     }
   }
-  
+  }
 }
